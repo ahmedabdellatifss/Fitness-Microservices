79338955d8ef48a20c0d8aaa23f632fd8a224820
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2025-06-19 16:29:31 +0300

    initiate user-service

---
+++ user-servic/src/main/java/com/fitness/user_servic/UserServicApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.user_servic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServicApplication.class, args);
+	}
+
+}
