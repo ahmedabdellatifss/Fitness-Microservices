7201f3f6939d312489b35e184f66c4ff4b7a9e58
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2025-07-14 12:01:23 +0300

    Generating AI Driven Recommendations

---
+++ aiservice/src/main/java/com/fitness/aiservice/model/Activity.java
@@ -0,0 +1,29 @@
+package com.fitness.aiservice.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+
+@Data
+public class Activity {
+
+    private String id;
+
+    private String userId;
+
+    private Integer duration;
+
+    private String Type;
+
+    private Integer caloriesBurned;
+
+    private LocalDateTime startTime;
+
+    private Map<String, Object> additionalMetrics;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
